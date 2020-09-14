a4df602cfd3600eba446fb9237a546c31ba43f05
AdamEL1 <adam.el-houssami@polymtl.ca>
2020-09-14 22:34:35 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -22,7 +22,7 @@ def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float
 
 
 def to_degrees(angle_rads: float) -> tuple:
-    deg = 
+    deg = (360/2)
     return 0.0, 0.0, 0.0
 
 
