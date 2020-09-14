90f29bd8bea55135682c734341a2e6a767a15afa
AdamEL1 <adam.el-houssami@polymtl.ca>
2020-09-14 22:39:00 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -25,7 +25,7 @@ def to_degrees(angle_rads: float) -> tuple:
     deg = (360/2*math.pi)*angle_rads
     minutes = 60*deg
     seconds = 60*minutes
-    return 0.0, 0.0, 0.0
+    return deg, 0.0, 0.0
 
 
 def to_celsius(temperature: float) -> float:
