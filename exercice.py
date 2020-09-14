667b14f4f07acbd2fe381d9bf859f731496ddb57
AdamEL1 <adam.el-houssami@polymtl.ca>
2020-09-14 22:31:00 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -17,7 +17,7 @@ def average(a: float, b: float, c: float) -> float:
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
-    deg = angle_degs + 
+    deg = angle_degs + (1/60)*
     return 0.0
 
 
