b72b3acc5529f1d59544d83842572c02a50418d2
AdamEL1 <adam.el-houssami@polymtl.ca>
2020-09-14 23:25:31 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -23,7 +23,7 @@ def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float
 
 def to_degrees(angle_rads: float) -> tuple:
     deg = 
-    return
+    return 
 
 
 def to_celsius(temperature: float) -> float:
